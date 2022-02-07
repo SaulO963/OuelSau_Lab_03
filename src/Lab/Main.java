0297405a2a122d519f486493a14a968d0570c07c
SaulO963 <saul.ouellet@gmail.com>
2022-02-07 17:54:36 -0500

    Added description

--- src/Lab/Main.java
+++ src/Lab/Main.java
@@ -1,3 +1,11 @@
+/**
+ * Name: Saul Ouellet
+ * Date: Feb 7tu 2022
+ * Description: Program creates a MyPoint class to simulate a coordinate on a 2D grid.
+ * Implements methods to print, calculate distance, find in an array, and
+ * determine if points make a line.
+ */
+
 package Lab;
 
 /***
